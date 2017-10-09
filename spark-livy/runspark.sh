9c839ac832628f78abf99c2e2517dddca97f80b7
cherrera2001 <chris.herrera07@gmail.com>
2017-10-09 17:49:40 -0500

    Fixing an issue where livy was not starting

---
+++ spark-livy/runspark.sh
@@ -0,0 +1,9 @@
+$SPARK_HOME/sbin/start-master.sh
+
+if [ -z ${HOST+x} ]; then
+  export LIBPROCESS_IP=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f8)
+else
+  export LIBPROCESS_IP=$HOST
+fi
+
+$LIVY_HOME/bin/livy-server $@
